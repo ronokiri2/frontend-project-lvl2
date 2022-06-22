2065d66d0648a5490c98608dfe5e6c5693820809
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-06-22 15:40:52 +0300

    added first step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
 console.log("Welcome to the gendiff.js file!")
+
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1.0');
+
+program.parse();
