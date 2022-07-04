54e0c4621785cc24a1b23f47a7040d4b9fa14739
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-07-04 14:36:10 +0300

    Добавил функцию нахождения одинаковых строк

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,4 +1,9 @@
 import * as fs from 'node:fs';
+import _ from 'lodash'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const gendiff = (filepath1, filepath2) => {
   const json1 = fs.readFileSync(filepath1);
@@ -6,6 +11,43 @@ const gendiff = (filepath1, filepath2) => {
   const obj1 = JSON.parse(json1);
   const obj2 = JSON.parse(json2);
   console.log(obj1);
-  return obj2;
+  console.log(obj2);
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  const uniqKeys = _.union(keys1, keys2);
+  console.log(uniqKeys);
+
+  let obj3 = '{'
+  for (const key of uniqKeys) {
+	console.log(obj1[key])
+	if (obj1[key] === obj2[key]) {
+		obj3 += `    ${key}: ${obj1[key]} \n`; // у меня тут получаются значения а нужны ключи
+	}
+  }
+  console.log(obj3)
+
+//   let obj3 = '{'
+
+// 	for (const [key, value] of Object.entries(obj1) ) {
+// 		if (obj1.key === obj2.key) {
+// 			obj3 += `    ${obj1.key}: ${obj1.value}\n`;
+// 		}
+// 	}
+
+// 	obj3 += '}'
+// 	console.log(obj3);
+
+	// полученный JSON нужно сначала преобразовать в объект. Дальше хорошим решением будет сначала найти объединение всех ключей (то есть получить массив всех ключей, которые есть и в первом и во втором объекте). А дальше уже пробежаться по этому массиву и смотреть, что стало с каждым из ключей. 
+	// Если ключ есть в первом объекте, но отсутствует во втором - значит удалён. Если отсутствует в первом, но есть во втором - добавлен. И так далее
+
+  return 'end';
 };
+
+
+
+
+
+
+
 export default gendiff;
