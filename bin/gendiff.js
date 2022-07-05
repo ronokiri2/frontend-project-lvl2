a2c4ed4e032630eac7ba4381a3f76f67a739bfd5
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-07-05 12:50:58 +0300

    Вывел код для определения одинаковых значений ключа в отдельную функцию

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,15 +5,15 @@ import gendiff from '../src/gendiff.js';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.5.0')
+  .version('0.6.0')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-	console.log(gendiff(filepath1, filepath2))
+    console.log(gendiff(filepath1, filepath2));
   });
 
 program.parse();
 
 // название у этого файла такое же как и у файла папке src
 // попробовал сменить название и функция перестала работать
-// хотя я также поменял путь к файлу в package.json, package-lock.json и Makefile 
+// хотя я также поменял путь к файлу в package.json, package-lock.json и Makefile
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,20 +1,24 @@
 import * as fs from 'node:fs';
-import _ from 'lodash'
+import _ from 'lodash';
 
 // не понятно как этим пользоваться
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 // работает только с файлами file1.json и file2.json
-// не знаю как сделать так, чтобы функция работала и с другими файлами 
+// не знаю как сделать так, чтобы функция работала и с другими файлами
 
 // Воспользовался советом техподдержки в обсуждениях:
 
-// полученный JSON нужно сначала преобразовать в объект. Дальше хорошим решением будет сначала найти объединение всех ключей (то есть получить массив всех ключей, которые есть и в первом и во втором объекте). А дальше уже пробежаться по этому массиву и смотреть, что стало с каждым из ключей. 
-// Если ключ есть в первом объекте, но отсутствует во втором - значит удалён. Если отсутствует в первом, но есть во втором - добавлен. И так далее
+// полученный JSON нужно сначала преобразовать в объект.
+// Дальше хорошим решением будет сначала найти объединение всех ключей
+// (то есть получить массив всех ключей, которые есть и в первом и во втором объекте).
+// А дальше уже пробежаться по этому массиву и смотреть, что стало с каждым из ключей.
+// Если ключ есть в первом объекте, но отсутствует во втором - значит удалён.
+// Если отсутствует в первом, но есть во втором - добавлен. И так далее
+
 
 const gendiff = (filepath1, filepath2) => {
   const json1 = fs.readFileSync(filepath1);
@@ -27,39 +31,61 @@ const gendiff = (filepath1, filepath2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const uniqKeys = _.union(keys1, keys2);
-//   console.log(uniqKeys);
+  //   console.log(uniqKeys);
 
-  let obj3 = '{ \n'
-  for (const key of uniqKeys) {
-    // console.log(obj1[key])
 
-	if (obj1[key] === obj2[key]) {
-	  obj3 += `    ${key}: ${obj1[key]} \n`;
-	}
+// функция для проверки наличия ключа
 
-	else if (obj1.hasOwnProperty(key) && !obj2.hasOwnProperty(key)) {
-	  obj3 += `  - ${key}: ${obj1[key]} \n`;
-	}
 
+  let obj3 = '{ \n';
+  uniqKeys.forEach((key) => {
+    // если значения ключей одинаковы
+    if (areValuesSame(obj1, obj2, key) === true) {
+      obj3 += `    ${key}: ${obj1[key]} \n`;
+    }
+	// если у объекта1 есть ключ и у объекта 2 нет ключа
+	else if (Object.prototype.hasOwnProperty.call(obj1, key) && !Object.prototype.hasOwnProperty.call(obj2, key)) {
+      obj3 += `  - ${key}: ${obj1[key]} \n`;
+    } 
+	// если у объекта1 нет ключа и у объекта 2 есть ключ
 	else if (!obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key)) {
-	  obj3 += `  + ${key}: ${obj1[key]} \n`;
-	}
-
+      obj3 += `  + ${key}: ${obj1[key]} \n`;
+    } 
+	
+	// если у объекта1 и объекта 2 есть ключи и их значения не одинаковы
 	else if (obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key) && obj1[key] !== obj2[key]) {
-	  obj3 += `  - ${key}: ${obj1[key]} \n`;
-	  obj3 += `  + ${key}: ${obj2[key]} \n`;
-	}
-  }
+      obj3 += `  - ${key}: ${obj1[key]} \n`;
+      obj3 += `  + ${key}: ${obj2[key]} \n`;
+    }
+  });
+  // чтобы избавиться от длинной строки и нечитаемого кода я предлагаю
+  // создать тернарную функцию obj1.hasOwnProperty(key)
+  //   for (const key of uniqKeys) {
+  //   // console.log(obj1[key])
+  //     if (obj1[key] === obj2[key]) {
+  //       obj3 += `    ${key}: ${obj1[key]} \n`;
+  //     } else if (obj1.hasOwnProperty(key) && !obj2.hasOwnProperty(key)) {
+  //       obj3 += `  - ${key}: ${obj1[key]} \n`;
+  //     } else if (!obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key)) {
+  //       obj3 += `  + ${key}: ${obj1[key]} \n`;
+  //     } else if (obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key) && obj1[key] !== obj2[key]) {
+  //       obj3 += `  - ${key}: ${obj1[key]} \n`;
+  //       obj3 += `  + ${key}: ${obj2[key]} \n`;
+  //     }
+  //   }
 
-  obj3 += '}'
+  obj3 += '}';
 
   return obj3;
 };
 
-
-
-
-
-
+// функция: значения ключей одинаковы?
+const areValuesSame = (obj1, obj2 , key) => {
+  if (obj1[key] === obj2[key]) {
+    return true
+  } else {
+    return false
+  }
+}
 
 export default gendiff;
