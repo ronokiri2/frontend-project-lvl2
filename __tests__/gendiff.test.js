643b7d030343f56388ba9a7068047e1337423cfb
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-07-06 17:17:56 +0300

    Шаг 4: улучшил тест

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,20 +1,20 @@
-import * as path from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
+import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
 import gendiff from '../src/gendiff';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('1', () => {
-  const json3 = readFile('file3.json');
-  const json4 = readFile('file4.json');
+const file1 = readFile('file1.json');
+const file2 = readFile('file2.json');
+const fileOutput = readFile('fileOutput.json');
+// const fileOutput = readFileSync(getFixturePath('fileOutput.json'));
+// const dataFileOutput = readFile(pathFileOutput, { encoding: 'utf8', flag: 'r' });
 
-  const obj3 = JSON.parse(json3);
-  const obj4 = JSON.parse(json4);
-
-  expect(gendiff(obj3, obj4)).toEqual('expected');
+test('gendiff file1 + file2 = fileOutput', () => {
+  expect(gendiff(file1, file2)).toBe(fileOutput);
 });
