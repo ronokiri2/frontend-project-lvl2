c423817126bc6ec0a8d1c39efd88e007558d3bf3
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-07-06 17:22:46 +0300

    Шаг 4: Тест заработал

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,12 +9,8 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-const file1 = readFile('file1.json');
-const file2 = readFile('file2.json');
 const fileOutput = readFile('fileOutput.json');
-// const fileOutput = readFileSync(getFixturePath('fileOutput.json'));
-// const dataFileOutput = readFile(pathFileOutput, { encoding: 'utf8', flag: 'r' });
 
 test('gendiff file1 + file2 = fileOutput', () => {
-  expect(gendiff(file1, file2)).toBe(fileOutput);
+  expect(gendiff('file1.json', 'file2.json')).toEqual(fileOutput);
 });
