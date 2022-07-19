f7708b3dcd62708475010dae284228d6f55a99c9
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-07-19 18:59:51 +0300

    Update createTxt.js

--- src/createTxt.js
+++ src/createTxt.js
@@ -7,45 +7,101 @@
 // ]
 
 import _ from 'lodash';
+
 // функция для преобразования внутреннего объекта в строку
-const stringifyObject = (nestedObject) => {
-  if (!_.isObject(nestedObject)) {
-    return nestedObject;
+// const stringifyObject = (obj) => {
+//   console.log(`PLAIN OBJECT KEY = ${obj.key}`);
+//   console.log(`PLAIN OBJECT STATUS = ${obj.status}`);
+//   console.log(`PLAIN OBJECT VALUE = ${obj.value}`);
+
+//   return Object.keys(obj).map((key) => ({ key, status: 'unchanged', value: obj[key] }));
+// };
+// const indent = (depthLevel, count = 4) => ' '.repeat(depthLevel * count - 2);
+
+// const stringify = (value, depth) => {
+//   if (!_.isPlainObject(value)) {
+//     return value;
+//   }
+
+//   const result = Object.entries(value).map(([key, content]) => {
+//     const handledContent = `${stringify(content, depth + 1)}`;
+//     return `${indent(depth + 1)}  ${key}: ${handledContent}`;
+//   });
+
+//   return `{\n${result.join('\n')}\n${indent(depth)}  }`;
+// };
+
+// const stringify = (value, depth) => {
+//   const space = '  ';
+//   const spaces = space.repeat(depth);
+//   const result = [];
+//   value.forEach((obj) => {
+//     if (obj.type === 'nested') {
+//       console.log('nested');
+//       // result += createNestedTxt(obj.value, depth + 4);
+//     } else if (_.isPlainObject(obj.value)) {
+//       console.log('plain');
+//     } else if (obj.type === 'unchanged') {
+//       result.push(`${spaces}  ${obj.key}: ${obj.value}\n`);
+//     } else if (obj.type === 'deleted') {
+//       result.push(`${spaces}- ${obj.key}: ${obj.value}\n`);
+//     } else if (obj.type === 'added') {
+//       result.push(`${spaces}+ ${obj.key}: ${obj.value}\n`);
+//     } else if (obj.type === 'changed') {
+//       result.push(`${spaces}- ${obj.key}: ${obj.valueBefore}\n`);
+//       result.push(`${spaces}+ ${obj.key}: ${obj.valueAfter}\n`);
+//     }
+//   });
+// };
+
+const indent = (depth) => '  '.repeat(depth);
+
+const stringify = (elem, depth) => {
+  if (!_.isObject(elem)) {
+    return elem;
   }
-  return Object.keys(nestedObject).map((key) => ({ key, status: 'unchanged', value: nestedObject[key] }));
+  const string = Object.entries(elem).map(([key, value]) => {
+    const a = `${indent(depth)}  ${key}: ${stringify(value, depth + 1)}`;
+    return a;
+  });
+  return string;
 };
 
-const createNestedTxt = (difference, depth = 1) => {
-  const space = '  ';
-  const spaces = space.repeat(depth);
-  const result = [];
-  difference.forEach((obj) => {
-    if (obj.type === 'unchanged') {
-      result.push(`${spaces}  ${obj.key}: ${obj.value}\n`);
-    } else if (obj.type === 'deleted') {
-      result.push(`${spaces}- ${obj.key}: ${obj.value}\n`);
-    } else if (obj.type === 'added') {
-      result.push(`${spaces}+ ${obj.key}: ${obj.value}\n`);
-    } else if (obj.type === 'changed') {
-      result.push(`${spaces}- ${obj.key}: ${obj.valueBefore}\n`);
-      result.push(`${spaces}+ ${obj.key}: ${obj.valueAfter}\n`);
-    } else if (obj.type === 'nested') {
-      result.push(`${spaces}  ${obj.key}: {\n`);
-      const a = createNestedTxt(obj.children, depth + 1);
-      // console.log('top');
-      result.push(a);
-      result.push(`${spaces}\n`);
-      // result += createNestedTxt(obj.value, depth + 4);
-    } else if (_.isPlainObject(obj.children)) {
-      result.push(`${spaces}  ${obj.key}: {\n`);
-      const b = createNestedTxt(stringifyObject(obj.value, depth + 1));
-      result.push(b);
-      result.push(`${spaces}\n`);
-      // result += createNestedTxt(obj.value, depth + 4);
-    }
-  });
-  result.push('}');
-  return result.join('');
+const renderDiff = (obj, depth) => {
+  if (obj.type === 'unchanged') {
+    return `${indent(depth)}  ${obj.key}: ${stringify(obj.value, depth + 1)}`;
+  } if (obj.type === 'deleted') {
+    return `${indent(depth)}- ${obj.key}: ${stringify(obj.value, depth + 1)}`;
+  } if (obj.type === 'added') {
+    return `${indent(depth)}+ ${obj.key}: ${stringify(obj.value, depth + 1)}`;
+  } if (obj.type === 'changed') {
+    return `${indent(depth)}- ${obj.key}: ${stringify(obj.valueBefore, depth + 1)}\n${indent(depth)}+ ${obj.key}: ${stringify(obj.valueAfter, depth + 1)}`;
+  } if (obj.type === 'nested') {
+    const nestedChild = obj.children.map((child) => renderDiff(child, depth + 2));
+    const nestedChild2 = nestedChild.join('\n');
+    console.log(`nestedChild2 = ${nestedChild2}!`);
+    const trimmedNestedChild = nestedChild2.trim();
+    console.log(`trimmedNestedChild = ${trimmedNestedChild}`);
+    const nestedChild3 = `${trimmedNestedChild}\n${indent(depth)}}`;
+    // console.log('nestedChild3');
+    // console.log(nestedChild3);
+    return `${indent(depth + 2)}+ ${obj.key}: {\n${nestedChild3}`;
+  }
+return 'x';
+  // result.unshift(`${indent(depth - 1)}{\n`);
+  // result.push(`${indent(depth - 1)}}`);
+  // return result.join('');
+  // const result2 = result.unshift('{\n');
+  // const result3 = result2.join('');
+  // return result3;
+};
+
+const createNestedTxt = (difference) => {
+  // console.log(difference);
+  const map = difference.map((elem) => renderDiff(elem, 1));
+  const map2 = map.join('\n');
+  const map3 = `{\n${map2}\n}`;
+  return map3;
 };
 
 export default createNestedTxt;
