abff0562eddcc0b1d50abe11cf0d77fdf3bcd072
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-07-25 15:56:54 +0300

    Шаг 6: Убрал несколько ошибок

--- src/createTxt.js
+++ src/createTxt.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 
 const spaces = (depth) => {
-  const symbol = '  ';
-  const space = symbol.repeat(depth);
+  const symbol = ' ';
+  const space = symbol.repeat(depth * 4 - 2);
   return space;
 };
 
@@ -12,9 +12,11 @@ const stringify = (elem, depth) => {
   }
   const str = Object.entries(elem).map(([key, value]) => {
     const stringifiedValue = stringify(value, depth + 1);
-    return `{\n${spaces(depth + 1)}${key}: ${stringifiedValue}\n${spaces(depth)}}`;
+    const stringWithKey = `${spaces(depth + 1)}  ${key}: ${stringifiedValue}`;
+    return stringWithKey;
   });
-  return str;
+  const stringWithBrackets = `{\n${str}\n${spaces(depth)}}`;
+  return stringWithBrackets;
 };
 
 const createNestedTxt = (difference) => {
@@ -25,27 +27,30 @@ const createNestedTxt = (difference) => {
       case 'root':
         return `{\n${node.children.map((child) => iter(child, depth + 1)).join('\n')}\n}`;
 
-      case 'nested':
-        const value = `\n${node.children.map((child) => iter(child, depth + 1)).join('\n')}}`;
-        return `${spaces(depth)}  ${node.key}: ${value}`;
-
-      case 'added':
+      case 'nested': {
+        // const value = `\n${node.children.map((child) => iter(child, depth + 1)).join('\n')}}`;
+        const value = node.children.map((child) => iter(child, depth + 1));
+        const joinedValue = value.join('\n');
+        const valueWithBrackets = `{\n${joinedValue}\n${spaces(depth)}  }`;
+        return `${spaces(depth)}  ${node.key}: ${valueWithBrackets}`;
+      }
+      case 'added': {
         const value2 = stringify(node.value, depth);
         return (`${spaces(depth)}+ ${node.key}: ${value2}`);
-
-      case 'deleted':
+      }
+      case 'deleted': {
         const value3 = stringify(node.value, depth);
         return (`${spaces(depth)}- ${node.key}: ${value3}`);
-
-      case 'unchanged':
+      }
+      case 'unchanged': {
         const value4 = stringify(node.value, depth);
         return (`${spaces(depth)}  ${node.key}: ${value4}`);
-
-      case 'changed':
+      }
+      case 'changed': {
         const valueBefore = stringify(node.valueBefore, depth);
         const valueAfter = stringify(node.valueAfter, depth);
         return (`${spaces(depth)}- ${node.key}: ${valueBefore}\n${spaces(depth)}+ ${node.key}: ${valueAfter}`);
-
+      }
       default:
         console.log('error in switch case');
     }
